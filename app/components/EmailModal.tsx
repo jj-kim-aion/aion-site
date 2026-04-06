05b6f7e401d4f392ef436b7859b1d44a078476e8
Shashith <Shashith.Aravinda@cloudsolutions.com.sa>
2026-04-06 12:17:02 +0530

    store page enhancements

--- app/components/EmailModal.tsx
+++ app/components/EmailModal.tsx
@@ -88,7 +88,7 @@ export function EmailModal({
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center p-4"
+      className="fixed inset-0 z-[100] flex items-center justify-center p-4"
       onClick={onClose}
     >
       {/* Backdrop */}
